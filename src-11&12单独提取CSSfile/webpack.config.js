6222c43c706c37082c365754201dc0580098472c
BDFD <bdfd2005@gmail.com>
2020-04-17 17:52:57 -0400

    update 11 & 12

---
+++ src-11&12单独提取CSSfile/webpack.config.js
@@ -0,0 +1,59 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/build.js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 3 * 1024,
+          esModule: false,
+          name: '[hash:10].[ext]',
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        exclude: /\.(css|js|html|less|jpg|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[hash:10].[ext]',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/build.css',
+    }),
+  ],
+  mode: 'development', //开发模式
+  // devServer: {
+  //   contentBase: resolve(__dirname, 'build'),
+  //   compress: true,
+  //   port: 3000,
+  //   open: true,
+  // },
+};
