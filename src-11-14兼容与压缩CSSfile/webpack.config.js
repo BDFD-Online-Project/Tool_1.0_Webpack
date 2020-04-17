31e0e9345b87025868c6705b610fa46ae4c7c7c7
BDFD <bdfd2005@gmail.com>
2020-04-17 19:08:24 -0400

    update 11-14

--- src-11-14兼容与压缩CSSfile/webpack.config.js
+++ src-11-14兼容与压缩CSSfile/webpack.config.js
@@ -1,6 +1,9 @@
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+//添加nodejs环境变量参数
+process.env.NODE_ENV = 'development';
 
 module.exports = {
   entry: './src/js/index.js',
@@ -12,7 +15,21 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          /*
+          css兼容性处理 postcss -> postcss-loader postcss-preset-env
+          另外需要在package.json中BroweserList里面的配置，通过配置加载指定的css兼容性样式
+          */
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [require('postcss-preset-env')()],
+            },
+          },
+        ],
       },
       {
         test: /\.less$/,
@@ -48,12 +65,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'css/build.css',
     }),
+    //压缩css
+    new OptimizeCssAssetsWebpackPlugin({}),
   ],
   mode: 'development', //开发模式
-  // devServer: {
-  //   contentBase: resolve(__dirname, 'build'),
-  //   compress: true,
-  //   port: 3000,
-  //   open: true,
-  // },
 };
