99c67cef55fbecb4b698df1b7d13380abb2b73eb
BDFD <bdfd2005@gmail.com>
2020-04-23 12:59:30 -0400

    update 10 Part 2

---
+++ src-10Part1-2SeperateFileIntoFolder/webpack.config.js
@@ -0,0 +1,57 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "build.js",
+    path: resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: "url-loader",
+        options: {
+          limit: 3 * 1024,
+          esModule: false,
+          name: "[hash:10].[ext]",
+          outputPath: "images",
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        exclude: /\.(css|js|html|less|jpg|png|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: "[hash:10].[ext]",
+          outputPath: "media",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  mode: "development", //开发模式
+  devServer: {
+    contentBase: resolve(__dirname, "build"),
+    compress: true,
+    port: 3000,
+    open: true,
+  },
+};
