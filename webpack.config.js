01f3540de4bf580ceb16dfaa4b2ed95d1276a74b
BDFD <bdfd2005@gmail.com>
2020-04-24 16:24:27 -0400

    1

--- webpack.config.js
+++ webpack.config.js
@@ -5,109 +5,104 @@ const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plug
 
 process.env.NODE_ENV = "development";
 
-module.exports = {
-  entry: "./src/js/index.js",
-  output: {
-    filename: "js/build.js",
-    path: resolve(__dirname, "build"),
+const commonCSSloader = [
+  {
+    loader: MiniCssExtractPlugin.loader,
+    options: {
+      publicPath: "../",
+    },
   },
+  "css-loader",
+  {
+    loader: "postcss-loader",
+    options: {
+      ident: "postcss",
+      plugins: () => [require("postcss-preset-env")()],
+    },
+  },
+  "less-loader",
+];
+
+module.exports = {
+  entry: {
+    //单入口：使用与SPA
+    main: "./src/js/index.js",
+    //多入口：有一个入口最终输出就有一个bundle
+    // main: "./src/js/index.js",
+    // test: "./src/js/test.js",
+  },
+
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
+        oneOf: [
           {
-            loader: "babel-loader",
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: [
+              {
+                loader: "babel-loader",
+                options: {
+                  presets: [
+                    [
+                      "@babel/preset-env",
+                      {
+                        useBuiltIns: "usage",
+                        corejs: {
+                          version: 3,
+                        },
+                        targets: {
+                          chrome: "60",
+                          firefox: "50",
+                          ie: "8",
+                          safari: "10",
+                          edge: "17",
+                        },
+                      },
+                    ],
+                  ],
+                  cacheDirectory: true,
+                },
+              },
+              {
+                loader: "eslint-loader",
+                options: {
+                  fix: true,
+                },
+              },
+            ],
+          },
+          {
+            test: /\.css$/,
+            use: [...commonCSSloader],
+          },
+          {
+            test: /\.less$/,
+            use: [...commonCSSloader, "less-loader"],
+          },
+          {
+            test: /\.(jpg|png|gif)$/,
+            loader: "url-loader",
             options: {
-              presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    useBuiltIns: "usage",
-                    corejs: {
-                      version: 3,
-                    },
-                    targets: {
-                      chrome: "60",
-                      firefox: "50",
-                      ie: "8",
-                      safari: "10",
-                      edge: "17",
-                    },
-                  },
-                ],
-              ],
+              limit: 3 * 1024,
+              esModule: false,
+              name: "[hash:10].[ext]",
+              outputPath: "images",
             },
           },
           {
-            loader: "eslint-loader",
+            test: /\.html$/,
+            loader: "html-loader",
+          },
+          {
+            exclude: /\.(css|js|html|less|jpg|png|gif)$/,
+            loader: "file-loader",
             options: {
-              fix: true,
+              name: "[hash:10].[ext]",
+              outputPath: "media",
             },
           },
         ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "../",
-            },
-          },
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: () => [require("postcss-preset-env")()],
-            },
-          },
-        ],
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "../",
-            },
-          },
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: () => [require("postcss-preset-env")()],
-            },
-          },
-          "less-loader",
-        ],
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        loader: "url-loader",
-        options: {
-          limit: 3 * 1024,
-          esModule: false,
-          name: "[hash:10].[ext]",
-          outputPath: "images",
-        },
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-      },
-      {
-        exclude: /\.(css|js|html|less|jpg|png|gif)$/,
-        loader: "file-loader",
-        options: {
-          name: "[hash:10].[ext]",
-          outputPath: "media",
-        },
       },
     ],
   },
@@ -117,9 +112,32 @@ module.exports = {
       template: "./src/index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: "./css/build.css",
+      filename: "./css/[name].[contenthash:10].css",
     }),
     new OptimizeCssAssetsWebpackPlugin({}),
   ],
-  mode: "development",
+  //可以自动将node_modules中代码单独打包一个chunk作为最终输出
+  //自动分析多入口chunk中公用文件，如果有会打包单独的chunk.有最小文件限制
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+
+  mode: "production",
+
+  output: {
+    //[name]: 取文件名
+    filename: "js/[name].[contenthash:10].js",
+    path: resolve(__dirname, "build"),
+  },
+
+  devServer: {
+    contentBase: resolve(__dirname, "build"),
+    compress: true,
+    port: 3000,
+    open: true,
+  },
+
+  devtool: "eval-source-map",
 };
