8ac80421381a655da2e8d1aef3d8aba43d9ed76f
BDFD <bdfd2005@gmail.com>
2020-04-23 20:08:16 -0400

    1

--- src/js/index.js
+++ src/js/index.js
@@ -3,27 +3,27 @@ import "../css/index.css";
 import "../css/index.less";
 import "../css/a.css";
 import "../css/b.css";
+import { mul } from "./test";
 
-function add1(x, y) {
-  return x + y;
-}
-
-const add2 = (x, y) => {
-  return x + y;
-};
-
-console.log(add1(2, 3));
-console.log(add2(3, 4));
-
-const promise = new Promise((resolve) => {
-  setTimeout(() => {
-    console.log("success");
-    resolve();
-  }, 1000);
-});
-console.log(promise);
-
-function sum(...args) {
-  return args.reduce((p, c) => p + c, 0);
-}
-console.log(sum(1, 2, 3, 4, 5));
+console.log("index.js is running...");
+// function add1(x, y) {
+//   return x + y;
+// }
+// const add2 = (x, y) => {
+//   return x + y;
+// };
+// console.log(add1(2, 3));
+// console.log(add2(3, 4));
+// const promise = new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log("success");
+//     resolve();
+//   }, 1000);
+// });
+// console.log(promise);
+// function sum(...args) {
+//   return args.reduce((p, c) => p + c, 0);
+// }
+// console.log(sum(1, 2, 3, 4, 5));
+console.log(mul(2, 3));
+console.log(mul(3, 3));
