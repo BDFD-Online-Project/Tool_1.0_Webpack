b82aee771f658084539a05d7ce934b438fd1cc74
BDFD <bdfd2005@gmail.com>
2020-04-24 15:56:01 -0400

    1

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -109,37 +109,5 @@ module.exports = {
     }),
     new OptimizeCssAssetsWebpackPlugin({}),
   ],
-  // mode: "production",
-  devServer: {
-    contentBase: resolve(__dirname, "build"),
-    compress: true,
-    port: 3000,
-    open: true,
-  },
-  /*
-  [inline-|hidden-|eval-][nosources-][cheap-[module-]]source-map
-  source-map:
-  显示错误代码准确信息和源代码的错误位置
-  inline-source-map:只生成一个内联，出现在build.js中
-  显示错误代码准确信息和源代码的错误位置
-  eval-source-map:每个文件有一个单独内联，都在eval中
-  显示错误代码准确信息和源代码的错误位置
-  hidden-source-map:外部
-  显示错误代码准确信息但不显示源代码位置
-  nosource-source-map:外部
-  显示错误代码准确信息但没有任何源代码信息
-  cheap-source-map:外部
-  显示错误代码准确信息和源代码的错误位置
-  但是只精确到列
-  cheap-module-source-map:外部
-  显示错误代码准确信息和源代码的错误位置
-  但是只精确到列，同时增加loader的source map加入其中
-  */
-  //开发环境：
-  //速度快： eval>inline>cheap
-  //调试友好： source-map<cheap-module<cheap
-  //生产环境：
-  //稳定：nosource-source-map 全部隐藏 /hidden-source-map 只隐藏原代码
-
-  devtool: "eval-source-map",
+  mode: "production",
 };
