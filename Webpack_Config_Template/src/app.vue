a710a2946e9be4e44a817fe39449809c992148ed
BDFD <bdfd2005@gmail.com>
2020-04-27 14:28:31 -0400

    set up basic vue structure

--- Webpack_Config_Template/src/app.vue
+++ Webpack_Config_Template/src/app.vue
@@ -1,24 +1,28 @@
 <template>
-    <div class="container"><span>{{msg}}</div>
+  <div class="container">
+    <span>{{msg}}</span>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return{
-            msg: 'Webpack 搭建Vue 环境基本完成'
-        }
-    }
-}
+  data() {
+    return {
+      msg: "Hello Wolrd From Back-End Side"
+    };
+  }
+};
 </script>
 
 <style>
-    .container{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size:20px;
-        color: lightblue;
-        box-shadow: 5px 5px 5px 5px;
-    }
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: lightblue;
+  box-shadow: 5px 5px 5px 5px;
+}
 </style>
